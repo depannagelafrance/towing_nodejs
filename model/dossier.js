e92fe83561251ba1003d803d66162a2d80b2548b
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2014-11-08 14:51:05 +0100

    added summary for internal notes

--- model/dossier.js
+++ model/dossier.js
@@ -1,4 +1,3 @@
-
 var db        = require('../util/database.js');
 var LOG       = require('../util/logger.js');
 
@@ -20,6 +19,8 @@ const SQL_FETCH_CAUSER_SIGNATURE            = "CALL R_FETCH_CAUSER_SIGNATURE_BY_
 const SQL_FETCH_COLLECTOR_SIGNATURE         = "CALL R_FETCH_COLLECTOR_SIGNATURE_BY_VOUCHER(?,?);";
 const SQL_FETCH_TRAFFIC_POST_SIGNATURE      = "CALL R_FETCH_TRAFFIC_POST_SIGNATURE_BY_VOUCHER(?,?);";
 
+const SQL_FETCH_COMM_AND_ATT_SUMMARY        = "CALL R_FETCH_COMM_AND_ATT_SUMMARY(?,?,?);";
+
 var findById = function($dossier_id, $token, cb) {
   var $dossier = {};
 
@@ -32,48 +33,63 @@ var findById = function($dossier_id, $token, cb) {
         $dossier.towing_company = $t_result;
     });
 
+
     db.many(SQL_FETCH_TOWING_VOUCHERS_BY_DOSSIER, [$dossier_id, $token],function($error, $v_result, $fields)
     {
       $vouchers = [];
 
       $v_result.forEach(function($voucher) {
+        var $dvt_params = [$dossier_id, $voucher.id, $token];
+        var $vt_params  = [$voucher.id, $token];
+
+        //settign the summary of the uploaded attachments and sent communications
+        db.many(SQL_FETCH_COMM_AND_ATT_SUMMARY, $dvt_params, function($error, $att_result, $fields) {
+            $_result = {};
+
+            $att_result.forEach(function($item) {
+                $_result[$item.type] = $item.number;
+            });
+
+            $voucher.communication_and_attachment_summary = $_result;
+        });
+
         //fetch the towing payments information
-        db.one(SQL_FETCH_TOWING_PAYMENTS_BY_VOUCHER, [$dossier_id, $voucher.id, $token], function($error, $p_result, $fields)
+        db.one(SQL_FETCH_TOWING_PAYMENTS_BY_VOUCHER, $dvt_params, function($error, $p_result, $fields)
         {
             $voucher.towing_payments = $p_result;
 
             //towing depot
-            db.one(SQL_FETCH_TOWING_DEPOT, [$voucher.id, $token], function($error, $t_result, $fields)
+            db.one(SQL_FETCH_TOWING_DEPOT, $vt_params, function($error, $t_result, $fields)
             {
                 $voucher.depot = $t_result;
 
                 //towing customer
-                db.one(SQL_FETCH_CUSTOMER, [$voucher.id, $token], function($error, $t_result, $fields)
+                db.one(SQL_FETCH_CUSTOMER, $vt_params, function($error, $t_result, $fields)
                 {
                     $voucher.customer = $t_result;
 
                     //towing causer
-                    db.one(SQL_FETCH_CAUSER, [$voucher.id, $token], function($error, $t_result, $fields)
+                    db.one(SQL_FETCH_CAUSER, $vt_params, function($error, $t_result, $fields)
                     {
                         $voucher.causer = $t_result;
 
                         //fetch the reference to the causer signature
-                        db.one(SQL_FETCH_CAUSER_SIGNATURE, [$voucher.id, $token], function($error, $t_result, $fields)
+                        db.one(SQL_FETCH_CAUSER_SIGNATURE, $vt_params, function($error, $t_result, $fields)
                         {
                             $voucher.signature_causer = $t_result;
 
                             //fetch the reference to the collector signature
-                            db.one(SQL_FETCH_COLLECTOR_SIGNATURE, [$voucher.id, $token], function($error, $t_result, $fields)
+                            db.one(SQL_FETCH_COLLECTOR_SIGNATURE, $vt_params, function($error, $t_result, $fields)
                             {
                                 $voucher.signature_collector = $t_result;
 
                                 //fetch the reference to the causer signature
-                                db.one(SQL_FETCH_TRAFFIC_POST_SIGNATURE, [$voucher.id, $token], function($error, $t_result, $fields)
+                                db.one(SQL_FETCH_TRAFFIC_POST_SIGNATURE, $vt_params, function($error, $t_result, $fields)
                                 {
                                     $voucher.signature_traffic_post = $t_result;
 
                                     //fetch the towing activities information
-                                    db.many(SQL_FETCH_TOWING_ACTIVITES_BY_VOUCHER, [$dossier_id, $voucher.id, $token], function($error, $a_result, $fields)
+                                    db.many(SQL_FETCH_TOWING_ACTIVITES_BY_VOUCHER, $dvt_params, function($error, $a_result, $fields)
                                     {
                                       $voucher.towing_activities = $a_result;
 
