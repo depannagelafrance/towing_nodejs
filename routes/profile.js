c65a9e617efd56eb55ee83c9426aa524b16fb111
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2014-11-20 14:27:34 +0100

    update profile

--- routes/profile.js
+++ routes/profile.js
@@ -13,10 +13,25 @@ var TAG = 'profile.js';
 var router = express.Router();
 
 //-- DEFINE CONSTANST
-const SQL_ADD_SIGNATURE_TO_USER_PROFILE                    = "CALL R_ADD_SIGNATURE_TO_USER_PROFILE(?,?,?,?);";
+const SQL_ADD_SIGNATURE_TO_USER_PROFILE   = "CALL R_ADD_SIGNATURE_TO_USER_PROFILE(?,?,?,?);";
+const SQL_UPDATE_PROFILE                  = "CALL R_UPDATE_USER_PROFILE(?,?,?,?,?);";
 
 
-//-- FETCH THE DOSSIERS
+//-- CREATE A SIGNATURE
+router.put('/:token', function($req, $res) {
+  var $token        = ju.requires('token', $req.params);
+
+  var $is_signa         = ju.requiresInt('is_signa', $req.body);
+  var $is_towing        = ju.requiresInt('is_towing', $req.body);
+  var $licence_plate    = ju.requires('licence_plate', $req.body);
+  var $registration_id  = ju.requires('registration_id', $req.body);
+
+  db.one(SQL_UPDATE_PROFILE, [$is_signa, $is_towing, $licence_plate, $registration_id, $token], function($error, $result, $fields) {
+      ju.send($req, $res, $result);
+  });
+});
+
+//-- CREATE A SIGNATURE
 router.post('/signature/:token', function($req, $res) {
   var $token        = ju.requires('token', $req.params);
 
